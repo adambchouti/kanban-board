783cf8ac025d976cdacfa2acefb0aaf7fa45f5a5
Adam <adambchouti93@gmail.com>
2020-08-29 12:43:11 -0400

    Footer and Navbar

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
